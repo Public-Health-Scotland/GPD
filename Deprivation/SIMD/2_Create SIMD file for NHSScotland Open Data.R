1b10418a8f8127a9a7d5e95b0233cf82345889a9
Calum Purdie <calump01@nss.scot.nhs.uk>
2019-12-18 12:15:33 +0000

    Add code for creating SIMD files

--- Deprivation/SIMD/2_Create SIMD file for NHSScotland Open Data.R
+++ Deprivation/SIMD/2_Create SIMD file for NHSScotland Open Data.R
@@ -1,53 +1,59 @@
-### 1 - Information ----
+##########################################################
+# Create SIMD file for NHSScotland Open Data
+# Calum Purdie
+# Original date 23/09/2019
+# Latest update author - Calum Purdie
+# Latest update date - 02/12/2019
+# Latest update description 
+# Type of script - Update
+# Written/run on RStudio Desktop
+# Version of R that the script was most recently run on - 3.5.1
+# Code to create Data Zone - SIMD lookup files for the NHSScotland open data 
+# platform
+# Approximate run time - <1 second
+##########################################################
 
-# Codename - Create SIMD file for NHSScotland Open Data
-# Data release - Scottish Index of Multiple Deprivation
-# Original Author - Calum Purdie
-# Original Date - 23/09/2019
-# Type - Creation
-# Written/run on - R Studio Desktop 
-# Version - 3.5.1
-#
-# install.packages("tidyr")
-# install.packages("dplyr")
-# install.packages("tidylog")
-#
-# Description - Code to create Data Zone - SIMD lookup files for the NHSScotland
-#               open data platform
-#
-# Approximate run time - <1 seconds
+### 1 - Housekeeping ----
 
-
-# Read in packages from library
-
+library(magrittr)
 library(tidyr)
 library(dplyr)
 library(tidylog)
+library(glue)
 
-# Update filepaths for new version
+# Set filepaths
 
 base_filepath <- file.path("//Freddy", "DEPT", "PHIBCS", "PHI")
+
 lookup_filepath <- file.path(base_filepath, "Referencing & Standards", "GPD",
-                             "3_Deprivation", "SIMD", "Lookup Files", "SIMD 2016", 
-                             "R Files")
+                             "3_Deprivation", "SIMD", "Lookup Files", 
+                             "SIMD 2016", "R Files")
+
 open_data_filepath <- file.path(base_filepath, "Publications", 
                                 "Open Data (Non Health Topic)", "Data", 
                                 "OD1700038 - SIMD")
 
+# Set date and file to use
+
+date <- strftime(Sys.Date(), format = "%d%m%Y")
+
+file <- "DataZone2011_simd2016"
+
 
 ### 2 - Read in lookup file ----
 
-datazone_simd <- readRDS(file.path(lookup_filepath, "DataZone2011_simd2016.rds")) %>% 
+datazone_simd <- readRDS(glue("{lookup_filepath}/{file}.rds")) %>% 
   select(DataZone2011:HB2019, HSCP2019, CA2019, simd2016rank:simd2016bt15)
 
-
 # Rename columns to match open data format
-colnames(datazone_simd) <- c("DZ2011", "IZ2011", "HB2014", "HSCP2016", "CA2011", "SIMD2016Rank", 
-                "SIMD2016CountryDecile", "SIMD2016CountryQuintile", 
-                "SIMD2016HB2014Decile", "SIMD2016HB2014Quintile", 
-                "SIMD2015HSCP2016Decile", "SIMD2016HSCP2016Quintile", 
-                "SIMD2016CA2011Decile", "SIMD2016CA2011Quintile", 
-                "SIMD2016Most15pc", "SIMD2016Least15pc")
+
+colnames(datazone_simd) <- c("DZ2011", "IZ2011", "HB2014", "HSCP2016", "CA2011", 
+                             "SIMD2016Rank", "SIMD2016CountryDecile", 
+                             "SIMD2016CountryQuintile", "SIMD2016HB2014Decile", 
+                             "SIMD2016HB2014Quintile", "SIMD2015HSCP2016Decile", 
+                             "SIMD2016HSCP2016Quintile", "SIMD2016CA2011Decile", 
+                             "SIMD2016CA2011Quintile", "SIMD2016Most15pc", 
+                             "SIMD2016Least15pc")
 
 
 ### 3 - Add Qualifier columns ----
@@ -57,7 +63,7 @@ colnames(datazone_simd) <- c("DZ2011", "IZ2011", "HB2014", "HSCP2016", "CA2011",
 # field is required to show which codes have been revised
 # Select the columns in the correct order for uploading
 
-datazone_simd <- datazone_simd %>% 
+datazone_simd %<>% 
   mutate(HB2014QF = case_when(HB2014 == "S08000029" | 
                               HB2014 == "S08000030" | 
                               HB2014 == "S08000031" | 
@@ -76,5 +82,4 @@ datazone_simd <- datazone_simd %>%
 
 ### 4 - Save file to open data folders ----
 
-# UPDATE FILEPATH WITH CURRENT DATE
-write_csv(datazone_simd, file.path(open_data_filepath, "simd2016_23092019.csv"))
+write_csv(datazone_simd, glue("{open_data_filepath}/simd2016_{date}.csv"))
