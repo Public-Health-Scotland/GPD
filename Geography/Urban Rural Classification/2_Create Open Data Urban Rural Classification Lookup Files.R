4d14c7f7c13fdc37d0ac5891146baf137a0730e9
Calum Purdie <calump01@nss.scot.nhs.uk>
2020-07-09 09:43:49 +0100

    Update urban rural script

--- Geography/Urban Rural Classification/2_Create Open Data Urban Rural Classification Lookup Files.R
+++ Geography/Urban Rural Classification/2_Create Open Data Urban Rural Classification Lookup Files.R
@@ -1,36 +1,40 @@
-### 1 - Information ----
-
-# Codename - Create Open Data Urban Rural Classification Lookup Files
-# Original Author - Calum Purdie
-# Original Date - 24/10/2019
-# Type - Creation
-# Written/run on - R Studio Desktop 
-# Version - 3.5.1
-#
-# install.packages("dplyr")
-# install.packages("readr")
-# install.packages("tidylog")
-# install.packages("glue")
-#
-# Description - Code to create Urban Rural Classification lookup files for the 
-#               NHSScotland Open Data platform
+##########################################################
+# Create Open Data Urban Rural Classification Lookup Files
+# Calum Purdie
+# Original date 24/10/2019
+# Latest update author - Calum Purdie
+# Latest update date - 08/07/2020
+# Latest update description - formatting code
+# Type of script - Creation
+# Written/run on RStudio Desktop
+# Version of R that the script was most recently run on - 3.5.1
+# Code to create Urban Rural Classification lookup files for the 
+# Scottish Health and Social Care Open Data platform
 # Approximate run time - <1 second
-
-od_filepath <- file.path("//Freddy", "DEPT", "PHIBCS", "PHI", "Publications", 
-                         "Open Data (Non Health Topic)", "Data", 
-                         "OD1800009 - Urban Rural")
-lookups_filepath <- file.path("//Freddy", "DEPT", "PHIBCS", "PHI", 
-                              "Referencing & Standards", "GPD", "1_Geography", 
-                              "Urban Rural Classification", "Lookup Files", 
-                              "R Files")
+##########################################################
 
 
-# Read in packages from library
+### 1 - Housekeeping ----
+
+# Read in packages
 
 library(dplyr)
 library(readr)
 library(tidylog)
 library(glue)
+
+# Set filepaths
+
+od_filepath <- glue("//Freddy/DEPT/PHIBCS/PHI/Publications/", 
+                    "Open Data (Non Health Topic)/Data/OD1800009 - Urban Rural")
+
+lookups_filepath <- glue("//Freddy/DEPT/PHIBCS/PHI/Referencing & Standards/", 
+                         "GPD/1_Geography/Urban Rural Classification/", 
+                         "Lookup Files/R Files")
+
+# Set date for filenames
+
+date <- strftime(Sys.Date(), format = "%d%m%Y")
 
 
 ### 2 - Data Zone Urban Rural Lookup ----
@@ -42,7 +46,7 @@ library(glue)
 DZ2011_UR_2016 <- readRDS(glue("{lookups_filepath}/", 
                                "DataZone2011_urban_rural_2016.rds")) %>% 
   select(DataZone2011, UR2_2016, UR3_2016, UR6_2016, UR8_2016) %>% 
-  rename(DZ2011 = DataZone2011, 
+  rename(DataZone = DataZone2011, 
          UrbanRural2fold2016 = UR2_2016, 
          UrbanRural3fold2016 = UR3_2016, 
          UrbanRural6fold2016 = UR6_2016, 
@@ -52,4 +56,4 @@ DZ2011_UR_2016 <- readRDS(glue("{lookups_filepath}/",
 # Update file name with the date the file is saved
 
 write_csv(DZ2011_UR_2016, 
-          glue("{lookups_filepath}/datazone2011_urban_rural_2016_241019.csv"))
+          glue("{od_filepath}/datazone2011_urban_rural_2016_{date}.csv"))
