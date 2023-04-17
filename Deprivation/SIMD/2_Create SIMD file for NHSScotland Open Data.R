7525c46ce72a20ef12418b65b6f520a11c62265a
Robert James Mitchell <robertjames.mitchell@phs.scot>
2023-04-17 13:37:35 +0100

    Code Restructure for "2_Create SIMD file for NHSScotland Open Data.R"

--- Deprivation/SIMD/2_Create SIMD file for NHSScotland Open Data.R
+++ Deprivation/SIMD/2_Create SIMD file for NHSScotland Open Data.R
@@ -13,7 +13,13 @@
 # Approximate run time - <1 second
 ##########################################################
 
+##########################################################
 ### 1 - Housekeeping ----
+##########################################################
+
+##############################################
+### 1.1 - Load Packages ----
+##############################################
 
 library(magrittr)
 library(tidyr)
@@ -22,7 +28,9 @@ library(tidylog)
 library(glue)
 library(readr)
 
-# Set filepaths
+##############################################
+### 1.2 -  Set filepaths ---- 
+##############################################
 
 base_filepath <- glue("//Freddy/DEPT/PHIBCS/PHI")
 
@@ -32,22 +40,25 @@ lookup_filepath <- glue("{base_filepath}/Referencing & Standards/GPD/",
 open_data_filepath <- glue("{base_filepath}/Publications/", 
                            "Open Data (Non Health Topic)/Data/", 
                            "OD1700038 - SIMD")
-
-# Set date and file to use
+##############################################
+### 1.3 - Set date and file to use
+##############################################
 
 date <- strftime(Sys.Date(), format = "%d%m%Y")
 
 file <- "DataZone2011_simd2020v2"
 
-
+##########################################################
 ### 2 - Read in lookup file ----
+##########################################################
 
 datazone_simd <- readRDS(glue("{lookup_filepath}/{file}.rds")) %>% 
   select(datazone2011, intzone2011, hb2019, hscp2019, ca2019, 
          simd2020v2_rank:simd2020v2bt15)
 
+############################
 # Rename columns to match open data format
-
+############################
 colnames(datazone_simd) <- c("DataZone", "IntZone", "HB", "HSCP", "CA", 
                              "SIMD2020V2Rank", "SIMD2020V2CountryDecile", 
                              "SIMD2020V2CountryQuintile", "SIMD2020V2HBDecile", 
@@ -56,15 +67,17 @@ colnames(datazone_simd) <- c("DataZone", "IntZone", "HB", "HSCP", "CA",
                              "SIMD2020V2CAQuintile", "SIMD2020V2Most15pc", 
                              "SIMD2020V2Least15pc")
 
-
-
+##########################################################
 ### 3 - Add Qualifier columns ----
+##########################################################
 
+############################
 # Due to minor boundary changes from 02/02/2018 (Keltybridge) and 01/04/2019 
 # (Cardowan by Stepps) which affeced some HB, HSCP and CA codes, a qualifier 
 # field is required to show which codes have been revised
 # Select the columns in the correct order for uploading
-# 
+############################
+
 # datazone_simd %<>% 
 #   mutate(HB2014QF = case_when(HB2014 == "S08000029" | 
 #                               HB2014 == "S08000030" | 
@@ -81,8 +94,15 @@ colnames(datazone_simd) <- c("DataZone", "IntZone", "HB", "HSCP", "CA",
 #   select(DZ2011:IZ2011, CA2011, CA2011QF, HSCP2016, HSCP2016QF, HB2014, HB2014QF, 
 #          everything())
 
-
+##########################################################
 ### 4 - Save file to open data folders ----
+##########################################################
 
 write_csv(datazone_simd, glue("{open_data_filepath}/simd2020V2_{date}.csv"), 
           na = "")
+
+##########################################################
+##########################################################
+################### End of script ---- ###################
+##########################################################
+##########################################################
